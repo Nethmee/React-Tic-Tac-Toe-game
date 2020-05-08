71734bbfd38e94cd0c54536f30c92e5291ff2d3c
Nethmee <nssellahewa@gmail.com>
2020-05-09 03:00:06 +0530

    Get winner function added

--- src/Board.js
+++ src/Board.js
@@ -30,9 +30,26 @@ class Board extends React.Component {
         })
         
     }
-  getWinner(){
+  getWinner(squares){
 
-      console.log('get winner function')
+      console.log('get winner function' +squares)
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null; 
         
     }
    
@@ -71,6 +88,8 @@ class Board extends React.Component {
     render() {
      const nextPlayer = (this.state.XIsNext)? "player 1":"player 2"
      console.log(nextPlayer)
+     const winner = this.getWinner(this.state.squares);
+     
  return (
 
     <div>
